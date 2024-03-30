2dd62166665997c4e2f3bb1a3e2092860fdbf4f9
maximosantiago <maximosantiagoroldanabiakel@gmail.com>
2024-03-30 13:57:12 -0300

    e s

--- Pokedex/settings.py
+++ Pokedex/settings.py
@@ -43,8 +43,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'PokedexApp',
     "Favoritos",
 ]
@@ -57,6 +55,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'Pokedex.urls'
